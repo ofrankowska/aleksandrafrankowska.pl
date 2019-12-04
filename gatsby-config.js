53f5ba37a0972e535ad5493bf319d1fc316caa00
Ola <olafrankowska112@gmail.com>
2019-12-04 22:23:03 +0100

    finished config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,48 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aleksandra Frankowska`,
+    description: `Aleksandra Frankowska - Frontend Developer Portofolio`,
+    author: `Aleksandra Frankowska`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-root-import`,
+      options: {
+        assets: path.join(__dirname, 'src/assets'),
+        components: path.join(__dirname, 'src/components'),
+        pages: path.join(__dirname, 'src/pages'),
+        utils: path.join(__dirname, 'src/utils'),
+        templates: path.join(__dirname, 'src/templates'),
+        src: path.join(__dirname, 'src/src'),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-styled-components',
+      options: {
+        displayName: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-eslint`,
+      test: /\.js$/,
+      exclude: /(node_modules|cache|public)/,
+      options: {
+        emitWarning: true,
+        failOnError: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-stylelint`,
+      options: { files: ['**/*.js'] },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -18,17 +50,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Aleksandra Frankowska`,
+        short_name: `aleksandrafrankowska`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/cute.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
